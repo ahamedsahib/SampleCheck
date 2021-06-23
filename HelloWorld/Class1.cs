5ab045074c76eefd6d3d01a8878e9b29304ed54b
ahamed <adsahib39@gmail.com>
2021-06-23 12:14:52 +0530

    [Ahamed] added new file

---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Sample1
+    {
+         public int Multiplication(int m1,int m2)
+        {
+            return m1 * m2;
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample1 obj = new Sample1();
+            Console.WriteLine("Answer is " + obj.Multiplication(5, 3));
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
